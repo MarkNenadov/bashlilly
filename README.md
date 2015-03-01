1b8b4b38cda643314168ba94325fbb4b38e22e87
Mark <marknenadov@gmail.com>
2015-02-28 20:28:52 -0500

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # bashlilly
-A Bash scripting function library.
+A Bash scripting function library by Mark Nenadov (2015) - marknenadov@gmail.com
+
+Usage
+-----
+
+To include bashlilly in a script, do the following:
+
+`source "/path/to/bashlilly.sh"`
+
+Functions
+=========
+
+safe_move 
+---------
+-> A "do no harm" move function which returns "true" if the move occurred or "false" if it didn't
+
+Sample Script:
+
+```
+#!/bin/bash
+
+source "/usr/local/bashlilly/bashlilly.sh"
+
+safe_move /home/bob/user_list.txt /home/bob/user_list.txt.old
+```
---
+++ src/files.sh
@@ -0,0 +1,37 @@
+# bashlilly - A Bash scripting function library
+#
+# by Mark Nenadov (2015)
+# marknenadov@gmail.com
+#
+# LICENSING
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version
+# This program is distributed in the hope that it will be useful
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>. 
+
+
+# safe_move - Move a file with a "do no harm" strategy
+# - example: safe_move from_file to_file
+# - return: "true" or "false"
+
+function safe_move() {
+        local isMoved="false"
+        local fromFile="$1"
+        local toFile="$2"
+
+        if [ -e "$fromFile" ] && ! [ -e "$toFile" ]; then
+        	mv ${fromFile} ${toFile}
+		
+		if ! [ -e "$fromFile" ] && [ -e "$toFile" ]; then
+                        isMoved="true"
+                fi
+        fi
+
+        echo "${isMoved}"
+}
