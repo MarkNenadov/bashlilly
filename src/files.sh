d8c3db0f26ad7178da8b51e908809316dd76ffcf
Mark <marknenadov@gmail.com>
2015-02-28 21:49:36 -0500

    ..

--- src/files.sh
+++ src/files.sh
@@ -16,10 +16,6 @@
 # along with this program. If not, see <http://www.gnu.org/licenses/>. 
 
 
-# safe_move - Move a file with a "do no harm" strategy
-# - example: safe_move from_file to_file
-# - return: "true" or "false"
-
 function exists() {
 	local thisFile="$1"
 
